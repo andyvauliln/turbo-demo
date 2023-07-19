cb24aa1d9605c22ede03fe66a5f0a80ba6e6586a
Andrei Vaulin <andy.vaulin@gmail.com>
2023-07-19 09:41:39 +0800

    config

--- apps/web/pages/_app.tsx
+++ apps/web/pages/_app.tsx
@@ -1,6 +1,6 @@
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function MyApp2({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
