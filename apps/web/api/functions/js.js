30ac033297372a9e4eed6261b6d25fd06b3b59c5
Andrei Vaulin <andy.vaulin@gmail.com>
2023-07-19 09:09:35 +0800

    install

--- apps/web/api/functions/js.js
+++ apps/web/api/functions/js.js
@@ -1,20 +1,13 @@
 const { json } = require('stream/consumers');
-const axios = require('axios');
 const base64 = require('base-64');
 
 const githubToken = process.env.GITHUB_TOKEN
 const repoOwner = 'andyvauliln'; // Replace with the repository owner's username
 const repoName = 'turbo-demo'; // Replace with the repository name
-const filePath = 'apps/web/api/functions/new.js';
+const filePath = 'apps/web/api/functions/new2.js';
 
-console.log(process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG, 'data');
-console.log(process.env.NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER, 'data');
-console.log(process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF, 'data');
-console.log(process.env.NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA, 'data');
-console.log(process.env.NEXT_PUBLIC_VERCEL_URL, 'data');
-console.log(process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL, 'data');
-console.log(process.env.GITHUB_TOKEN, 'token');
-console.log(process.env, 'ENV');
+console.log(process.env, 'data');
+// console.log(process.env, 'ENV');
 
 const jsCode = `
 export default function handler(req, res) {
@@ -25,33 +18,30 @@ export default function handler(req, res) {
 
 export default async function handler(req, res) {
     let str = "ok"
-    console.log('2', 'data');
     try {
         await commitFileToGithub(str);
         res.end(str)
     } catch (error) {
         console.log(error, "error")
-        res.end(JSON.stringify(error))
+        res.end("not ok")
+
     }
 }
 
 async function commitFileToGithub(str) {
     try {
-        const response = await axios({
-            method: 'put',
-            url: `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`,
+        const response = await fetch(`https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`, {
+            method: 'PUT',
             headers: {
                 Authorization: `token ${githubToken}`,
                 'Content-Type': 'application/json'
             },
-            data: {
+            body: JSON.stringify({
                 message: 'Created new file via API',
                 content: base64.encode(jsCode)
-            }
+            })
         });
-        console.log('3', response, 'data');
-        str += JSON.stringify(response.data)
-        console.log(response.data);
+        console.log('3', response, 'data');;
     } catch (error) {
         console.log(error);
     }
---
+++ apps/web/nodes/node-maneger.js
@@ -0,0 +1,53 @@
+export function configureNode() {
+
+}
+
+export function runNode() {
+
+}
+
+export function getNode(name) {
+
+
+}
+
+export function developeNode() {
+
+
+}
+
+export function saveNode() {
+
+}
+
+export function searchInternet() {
+
+}
+
+export function getAllNodes() {
+
+}
+
+export function searchNodes() {
+
+}
+
+export function changeModel() {
+
+}
+
+export function callModel() {
+
+}
+
+export function testNode() {
+
+}
+
+export function updateDevData() {
+
+}
+
+export function createGithubIssue() {
+
+}
